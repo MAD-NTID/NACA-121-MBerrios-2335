f916190b9cca7845e7a061b6e81bba7b15ddedc9
Michael Berrios <mxbics@rit.edu>
2024-02-21 11:49:12 -0500

    Added Whiteboard ICE

--- Unit-15-GroupActivity-ICE/obj/Debug/net8.0/Unit-15-GroupActivity-ICE.AssemblyInfo.cs
+++ Unit-15-GroupActivity-ICE/obj/Debug/net8.0/Unit-15-GroupActivity-ICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unit-15-GroupActivity-ICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+198a418eb0534dc006dee7774f24dac0cd2758ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac3d28b775dff60b32a5fde1020131aebac59c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unit-15-GroupActivity-ICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unit-15-GroupActivity-ICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
