fae2202990ff25f1fb4c1914a43e90f9e87919d8
Michael Berrios <mxbics@rit.edu>
2024-04-17 11:26:25 -0400

    Completed Packages Demo

---
+++ MyPackage/FirstPackageClass.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPackage;
+
+public class FirstPackageClass
+{
+    public static string GetHelloString(string name)
+    {
+        return $"Hello World! My name is {name}";
+    }
+}
---
+++ Packages/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using NAudio.Wave;
+
+using (var audioFile = new AudioFileReader("Recording.m4a"))
+using(var outputDevice = new WaveOutEvent())
+{
+    outputDevice.Init(audioFile);
+    outputDevice.Play();
+
+    Console.WriteLine("Audio is Playing... put your volume up");
+
+    while (outputDevice.PlaybackState == PlaybackState.Playing)
+    {
+        Thread.Sleep(1000);
+    }
+
+    Console.WriteLine("Audio Finished Playing");
+}
